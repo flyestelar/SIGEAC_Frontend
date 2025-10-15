636705e4c631aeff5a5b18e1b702eb52f07ee0c1
Angel Anton <aanton@transmandu.com>
2025-10-15 11:20:45 -0400

    push de columnas despachadas 7

--- hooks/mantenimiento/almacen/articulos/useGetArticleById.ts
+++ hooks/mantenimiento/almacen/articulos/useGetArticleById.ts
@@ -1,47 +1,16 @@
+import { EditingArticle } from '@/components/forms/mantenimiento/almacen/RegisterArticleForm';
 import axios from '@/lib/axios';
-import { Article, Batch, Convertion } from '@/types';
 import { useQuery } from '@tanstack/react-query';
-interface EditingArticle extends Article {
-  batches: Batch,
-  tool?: {
-    id: number,
-    serial: string,
-    isSpecial: boolean,
-    article_id: number,
-  },
-  component?: {
-    serial: string,
-    hard_time: {
-      hour_date: string,
-      cycle_date: string,
-      calendary_date: string,
-    },
-    shell_time: {
-      caducate_date: string,
-      fabrication_date: string,
-    }
-  },
-  consumable?: {
-    article_id: number,
-    is_managed: boolean,
-    convertions: Convertion[],
-    quantity: number,
-    shell_time: {
-      caducate_date: Date,
-      fabrication_date: string,
-      consumable_id: string,
-    }
-  },
-}
+
 const fetchArticleById = async (id: string, company?: string): Promise<EditingArticle> => {
-  const {data} = await axios.get(`/${company}/article/${id}`);
+  const { data } = await axios.get(`/${company}/article/${id}`);
   return data;
 };
 
 export const useGetArticleById = (id: string, company?: string) => {
   return useQuery<EditingArticle>({
-    queryKey: ["article", id, company],
+    queryKey: ['article', id, company],
     queryFn: () => fetchArticleById(id, company),
-    enabled: !!id && !!company
+    enabled: !!id && !!company,
   });
 };
