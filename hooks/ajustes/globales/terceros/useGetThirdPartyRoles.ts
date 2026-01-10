d858d0c4022c788381ccba8e15a7e8f00421a843
Angel Anton <aanton@transmandu.com>
2026-01-10 08:50:18 -0400

    fix de manufacturer en dispatchs 3

--- hooks/ajustes/globales/terceros/useGetThirdPartyRoles.ts
+++ hooks/ajustes/globales/terceros/useGetThirdPartyRoles.ts
@@ -2,17 +2,17 @@
 
 import axiosInstance from '@/lib/axios';
 import { useCompanyStore } from '@/stores/CompanyStore';
-import { ThirPartyRole } from '@/types';
+import { ThirdPartyRole } from '@/types';
 import { useQuery } from '@tanstack/react-query';
 
-const fetchThirdPartyRoles = async (company: string | undefined): Promise<ThirPartyRole[]> => {
+const fetchThirdPartyRoles = async (company: string | undefined): Promise<ThirdPartyRole[]> => {
   const { data } = await axiosInstance.get(`/${company}/third-party-roles`);
   return data;
 };
 
 export const useGetThirdPartyRoles = () => {
   const { selectedCompany } = useCompanyStore();
-  return useQuery<ThirPartyRole[]>({
+  return useQuery<ThirdPartyRole[]>({
     queryKey: ['third-party-roles', selectedCompany?.slug],
     queryFn: () => fetchThirdPartyRoles(selectedCompany?.slug),
     staleTime: 1000 * 60 * 2, // 5 minutos
