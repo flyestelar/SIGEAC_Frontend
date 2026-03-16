e914c40ba5d50bfdad8454d4a789ab4c8e12ced7
Angel <angeljanton@gmail.com>
2026-03-16 12:51:24 -0400

    FIX UI DE COMRPAS

--- components/dropdowns/mantenimiento/compras/RequisitionDropdownActions.tsx
+++ components/dropdowns/mantenimiento/compras/RequisitionDropdownActions.tsx
@@ -1,38 +1,38 @@
-import { useDeleteRequisition, useUpdateRequisitionStatus } from "@/actions/mantenimiento/compras/requisiciones/actions"
+import {
+  useDeleteRequisition,
+  useUpdateRequisitionStatus,
+} from '@/actions/mantenimiento/compras/requisiciones/actions';
 import {
   DropdownMenu,
   DropdownMenuContent,
   DropdownMenuItem,
-  DropdownMenuTrigger
-} from "@/components/ui/dropdown-menu"
-import { useAuth } from "@/contexts/AuthContext"
-import { useCompanyStore } from "@/stores/CompanyStore"
-import { Requisition } from "@/types"
-import { ClipboardCheck, ClipboardX, Loader2, MoreHorizontal, Trash2, FileDown } from "lucide-react"
-import { useState } from "react"
-import { CreateQuoteForm } from "../../../forms/mantenimiento/compras/CreateQuoteForm"
-import { Button } from "../../../ui/button"
-import { Dialog, DialogContent, DialogDescription, DialogFooter, DialogHeader, DialogTitle } from "../../../ui/dialog"
-import LoadingPage from "../../../misc/LoadingPage"
-import { PDFDownloadLink } from "@react-pdf/renderer"
-import RequisitionReportPdf from "@/components/pdf/almacen/RequisitionReportPdf"
-
+  DropdownMenuTrigger,
+} from '@/components/ui/dropdown-menu';
+import { useAuth } from '@/contexts/AuthContext';
+import { useCompanyStore } from '@/stores/CompanyStore';
+import { Requisition } from '@/types';
+import { ClipboardCheck, ClipboardX, Loader2, MoreHorizontal, Trash2, FileDown } from 'lucide-react';
+import { useState } from 'react';
+import { CreateQuoteForm } from '../../../forms/mantenimiento/compras/CreateQuoteForm';
+import { Button } from '../../../ui/button';
+import { Dialog, DialogContent, DialogDescription, DialogFooter, DialogHeader, DialogTitle } from '../../../ui/dialog';
+import LoadingPage from '../../../misc/LoadingPage';
+import { PDFDownloadLink } from '@react-pdf/renderer';
+import RequisitionReportPdf from '@/components/pdf/almacen/RequisitionReportPdf';
 
 function transformApiData(apiData: any) {
   return {
     order_number: apiData.order_number,
     justification: apiData.justification,
-    company: "", // Add appropriate value
+    company: '', // Add appropriate value
     created_by: apiData.created_by.id.toString(),
-    tax: "0",
+    tax: '0',
     requested_by: apiData.requested_by,
     articles: apiData.batch.map((batch: any) => ({
       batch: batch.id.toString(),
       batch_name: batch.name,
       batch_articles: batch.batch_articles.map((article: any) => ({
-        part_number: article.article_part_number ||
-          article.article_alt_part_number ||
-          article.pma,
+        part_number: article.article_part_number || article.article_alt_part_number || article.pma,
         unit: article.unit,
         quantity: parseFloat(article.quantity),
         image: article.image || null,
@@ -42,40 +42,37 @@ function transformApiData(apiData: any) {
 }
 
 const RequisitionsDropdownActions = ({ req }: { req: Requisition }) => {
+  const { user } = useAuth();
 
-  const { user } = useAuth()
+  const [open, setOpen] = useState<boolean>(false);
 
-  const [open, setOpen] = useState<boolean>(false)
+  const [openDelete, setOpenDelete] = useState<boolean>(false);
 
-  const [openDelete, setOpenDelete] = useState<boolean>(false)
+  const [openConfirm, setOpenConfirm] = useState<boolean>(false);
 
-  const [openConfirm, setOpenConfirm] = useState<boolean>(false)
+  const [openReject, setOpenReject] = useState<boolean>(false);
 
-  const [openReject, setOpenReject] = useState<boolean>(false)
+  const { deleteRequisition } = useDeleteRequisition();
 
+  const { updateStatusRequisition } = useUpdateRequisitionStatus();
 
-  const { deleteRequisition } = useDeleteRequisition()
+  const { selectedCompany } = useCompanyStore();
 
-  const { updateStatusRequisition } = useUpdateRequisitionStatus()
-
-  const { selectedCompany } = useCompanyStore()
-
-  const userRoles = user?.roles?.map(role => role.name) || [];
+  const userRoles = user?.roles?.map((role) => role.name) || [];
 
   const initialData = transformApiData(req);
 
   if (!selectedCompany) {
-    return <LoadingPage />
+    return <LoadingPage />;
   }
 
   const handleDelete = async (id: number, company: string) => {
     await deleteRequisition.mutateAsync({
       id,
-      company: selectedCompany!.slug
+      company: selectedCompany!.slug,
     });
-    setOpenDelete(false)
-  }
-
+    setOpenDelete(false);
+  };
 
   const handleReject = async (id: number, updated_by: string, status: string, company: string) => {
     const data = {
@@ -85,10 +82,10 @@ const RequisitionsDropdownActions = ({ req }: { req: Requisition }) => {
     await updateStatusRequisition.mutateAsync({
       id,
       data,
-      company: selectedCompany!.slug
+      company: selectedCompany!.slug,
     });
-    setOpenReject(false)
-  }
+    setOpenReject(false);
+  };
 
   return (
     <>
@@ -100,30 +97,27 @@ const RequisitionsDropdownActions = ({ req }: { req: Requisition }) => {
           </Button>
         </DropdownMenuTrigger>
         <DropdownMenuContent align="center" className="flex gap-2 justify-center">
-          {
-            ((userRoles.includes("ANALISTA_COMPRAS")) || (userRoles.includes("SUPERUSER"))) && (
-              <>
-                {
-                  (req.status !== 'APROBADA' && req.status !== 'COTIZADO') && (
-                    <DropdownMenuItem disabled={req.status === 'APROBADO' || req.status === 'RECHAZADO'} className="cursor-pointer">
-                      <ClipboardCheck onClick={() => setOpenConfirm(true)} className='size-5' />
-                    </DropdownMenuItem>
-                  )
-                }
-                <DropdownMenuItem disabled={req.status === 'RECHAZADO'} onClick={() => setOpenReject(true)} className="cursor-pointer">
-                  <ClipboardX className="size-5" />
+          {(userRoles.includes('ANALISTA_COMPRAS') ||
+            userRoles.includes('SUPERUSER') ||
+            userRoles.includes('JEFE_COMPRAS')) && (
+            <>
+              {req.status !== 'APROBADA' && req.status !== 'COTIZADO' && (
+                <DropdownMenuItem
+                  disabled={req.status === 'APROBADO' || req.status === 'RECHAZADO'}
+                  className="cursor-pointer"
+                >
+                  <ClipboardCheck onClick={() => setOpenConfirm(true)} className="size-5" />
                 </DropdownMenuItem>
-              </>
-            )
-          }
-          <PDFDownloadLink
-            fileName={`${req.order_number}.pdf`}
-            document={<RequisitionReportPdf requisition={req} />}
-          >
-            <DropdownMenuItem className="cursor-pointer">
-              <FileDown className="size-5 text-blue-600 hover:text-blue-700" />
-            </DropdownMenuItem>
-          </PDFDownloadLink>
+              )}
+              <DropdownMenuItem
+                disabled={req.status === 'RECHAZADO'}
+                onClick={() => setOpenReject(true)}
+                className="cursor-pointer"
+              >
+                <ClipboardX className="size-5" />
+              </DropdownMenuItem>
+            </>
+          )}
           <DropdownMenuItem onClick={() => setOpenDelete(true)} className="cursor-pointer">
             <Trash2 className="size-5 text-red-500" />
           </DropdownMenuItem>
@@ -139,8 +133,16 @@ const RequisitionsDropdownActions = ({ req }: { req: Requisition }) => {
             </DialogDescription>
           </DialogHeader>
           <DialogFooter>
-            <Button type="button" variant={"destructive"} onClick={() => setOpenDelete(false)}>Cancelar</Button>
-            <Button onClick={() => handleDelete(req.id, selectedCompany!.slug)} disabled={deleteRequisition.isPending} className="bg-primary text-white">{deleteRequisition.isPending ? <Loader2 className="animate-spin size-4" /> : "Confirmar"}</Button>
+            <Button type="button" variant={'destructive'} onClick={() => setOpenDelete(false)}>
+              Cancelar
+            </Button>
+            <Button
+              onClick={() => handleDelete(req.id, selectedCompany!.slug)}
+              disabled={deleteRequisition.isPending}
+              className="bg-primary text-white"
+            >
+              {deleteRequisition.isPending ? <Loader2 className="animate-spin size-4" /> : 'Confirmar'}
+            </Button>
           </DialogFooter>
         </DialogContent>
       </Dialog>
@@ -161,18 +163,26 @@ const RequisitionsDropdownActions = ({ req }: { req: Requisition }) => {
         <DialogContent>
           <DialogHeader>
             <DialogTitle className="text-center text-3xl">Rechazar Requisición</DialogTitle>
-            <DialogDescription className="text-center">
-              ¿Estás seguro de rechazar esta requisición?
-            </DialogDescription>
+            <DialogDescription className="text-center">¿Estás seguro de rechazar esta requisición?</DialogDescription>
           </DialogHeader>
           <DialogFooter>
-            <Button onClick={() => handleReject(req.id, `${user?.first_name} ${user?.last_name}`, "RECHAZADO", selectedCompany!.slug)} disabled={updateStatusRequisition.isPending} className="bg-primary text-white">{updateStatusRequisition.isPending ? <Loader2 className="animate-spin size-4" /> : "Confirmar"}</Button>
-            <Button type="button" variant={"destructive"} onClick={() => setOpenReject(false)}>Cancelar</Button>
+            <Button
+              onClick={() =>
+                handleReject(req.id, `${user?.first_name} ${user?.last_name}`, 'RECHAZADO', selectedCompany!.slug)
+              }
+              disabled={updateStatusRequisition.isPending}
+              className="bg-primary text-white"
+            >
+              {updateStatusRequisition.isPending ? <Loader2 className="animate-spin size-4" /> : 'Confirmar'}
+            </Button>
+            <Button type="button" variant={'destructive'} onClick={() => setOpenReject(false)}>
+              Cancelar
+            </Button>
           </DialogFooter>
         </DialogContent>
       </Dialog>
     </>
-  )
-}
+  );
+};
 
-export default RequisitionsDropdownActions
+export default RequisitionsDropdownActions;
