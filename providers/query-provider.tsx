719f1c52cb79aa111119d30b88b7e2e2480a981d
David Brito <dbrito@transmandu.com>
2026-03-26 08:53:06 -0400

    Enhance QueryClientProvider with TypeScript global declaration and development environment check

--- providers/query-provider.tsx
+++ providers/query-provider.tsx
@@ -3,12 +3,24 @@
 import { ReactNode, useMemo } from 'react';
 import { QueryClient, QueryClientProvider as RQQueryClientProvider } from '@tanstack/react-query';
 
+// This code is only for TypeScript
+declare global {
+  interface Window {
+    __TANSTACK_QUERY_CLIENT__: import('@tanstack/query-core').QueryClient;
+  }
+}
+
 interface Props {
   children: ReactNode;
 }
 
 const QueryClientProvider = ({ children }: Props) => {
   const queryClient = useMemo(() => new QueryClient(), []);
+
+  if (typeof window !== 'undefined' && process.env.NODE_ENV === 'development') {
+    window.__TANSTACK_QUERY_CLIENT__ = queryClient;
+  }
+
   return <RQQueryClientProvider client={queryClient}>{children}</RQQueryClientProvider>;
 };
 
