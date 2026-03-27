5e8fde6230ad220d9765fe4a7a6d6efba22ee1f4
David Brito <dbrito@transmandu.com>
2026-03-27 15:23:44 -0400

    add company id header

--- lib/axios.ts
+++ lib/axios.ts
@@ -1,11 +1,12 @@
+import { useCompanyStore } from '@/stores/CompanyStore';
 import axios from 'axios';
-import Cookies from 'js-cookie'
+import Cookies from 'js-cookie';
 
 const axiosInstance = axios.create({
   baseURL: process.env.NEXT_PUBLIC_API_BASE_URL,
   withCredentials: true,
   headers: {
-    "skip_zrok_interstitial": true,
+    skip_zrok_interstitial: true,
   },
 });
 
@@ -14,6 +15,13 @@ axiosInstance.interceptors.request.use((config) => {
   if (token) {
     config.headers.Authorization = `${token}`;
   }
+
+  const { selectedCompany } = useCompanyStore();
+
+  if (selectedCompany) {
+    config.headers['x-sigeac-company-id'] = selectedCompany.id;
+  }
+
   return config;
 });
 
