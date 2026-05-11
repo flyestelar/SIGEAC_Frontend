074294059f0fc6237ac4aae26116f978c4e3e422
Alvaro Aguinagalde <alvaro.aguinagalde@gmail.com>
2026-05-11 14:51:25 -0400

    trick

--- app/[company]/sms/aeronautical/gestion_de_riesgos/reportes/_components/report-attachments-section.tsx
+++ app/[company]/sms/aeronautical/gestion_de_riesgos/reportes/_components/report-attachments-section.tsx
@@ -56,6 +56,7 @@ export function ReportAttachmentsSection({
                                         ) : null}
                                     </div>
 
+
                                     {url ? (
                                         <Button
                                             variant="outline"
